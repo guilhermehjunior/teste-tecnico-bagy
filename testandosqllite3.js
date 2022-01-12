fe9804c630151fb8ab9525e41033785b670e41c8
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2022-01-12 13:32:53 -0300

    criando querys para o database

--- testandosqllite3.js
+++ testandosqllite3.js
@@ -1,24 +1,126 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // open the database connection
-let db = new sqlite3.Database('./teste.db', (err) => {
+let db = new sqlite3.Database('./teste3.db', (err) => {
   if (err) {
     console.error(err.message);
   }
 });
 
+const imageAddress = 'https://a-static.mlcdn.com.br/618x463/caneta-azul-0-7-pilot-bps/pedagogica/o1129942/ec26bc3662b688e9d07d465161c2180f.jpg';
+
+const queryCreateTableClients =`
+  CREATE TABLE IF NOT EXISTS clientes(
+  id INTEGER,
+  nomeCompleto TEXT NOT NULL,
+  email TEXT NOT NULL,
+  cpf TEXT NOT NULL,
+  dataNascimento TEXT NOT NULL,
+  idEndereco INTEGER NOT NULL,
+  PRIMARY KEY (id)
+  FOREIGN KEY (idEndereco)
+    REFERENCES enderecos (id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);`;
+
+const queryCreateTableAddress = `
+  CREATE TABLE IF NOT EXISTS enderecos(
+  id INTEGER,
+  rua TEXT NOT NULL,
+  bairro TEXT NOT NULL,
+  cidade TEXT NOT NULL,
+  estado TEXT NOT NULL,
+  país TEXT NOT NULL,
+  cep TEXT NOT NULL,
+  número INTEGER NOT NULL,
+  PRIMARY KEY (id));`;
+
+const queryCreateTableProducts = `
+  CREATE TABLE IF NOT EXISTS produtos(
+  id INTEGER,
+  nome TEXT NOT NULL,
+  imagem TEXT,
+  descricao TEXT NOT NULL,
+  peso FLOAT,
+  preco FLOAT NOT NULL,
+  estoque INTEGER NOT NULL,
+  PRIMARY KEY (id));`;
+
+const queryCreateTableOrders = `
+  CREATE TABLE IF NOT EXISTS pedidos(
+  id INTEGER,
+  dataPedido TEXT NOT NULL,
+  parcelas TEXT NOT NULL,
+  idComprador INTEGER NOT NULL,
+  status TEXT NOT NULL,
+  PRIMARY KEY (id)
+  FOREIGN KEY (idComprador)
+    REFERENCES clientes (id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);`;
+
+const queryCreateTableOrdersProducts = `
+  CREATE TABLE pedidos_produtos(
+  idProduto INTEGER,
+  quantidade INTEGER NOT NULL,
+  idVenda INTEGER,
+  PRIMARY KEY (idProduto, idVenda),
+  FOREIGN KEY (idProduto) 
+     REFERENCES produtos (idProduto) 
+        ON DELETE CASCADE 
+        ON UPDATE NO ACTION,
+  FOREIGN KEY (idVenda) 
+     REFERENCES pedidos (idVenda) 
+        ON DELETE CASCADE 
+        ON UPDATE NO ACTION);`;
+
+const queryInsertAddress = `INSERT INTO enderecos(rua, bairro, cidade, estado, país, cep, número) VALUES
+  ('Santo Garbuio', 'Xaxim', 'Curitiba', 'PR', 'BR', '81710-320', 127),
+  ('Francisco Derosso', 'Xaxim', 'Curitiba', 'PR', 'BR', '82710-320', 3000),
+  ('Gabriel Freceiro', 'Cidade Alta', 'Medianeira', 'PR', 'BR', '85884000', 127)`;
+
+const queryInsertClients = `INSERT INTO clientes(nomeCompleto, email, cpf, dataNascimento, addressId) VALUES
+  ('Guilherme H Jr', 'guilherme@email.com', '99999999999', '12/07/1992', 1),
+  ('Alceu Fonseca', 'fonseca@email.com', '88888888888', '20/09/1990', 2),
+  ('Rogerio Andrade', 'rogerio@email.com', '77777777777', '12/02/1980', 3)`;
+
+const queryInsertProducts = `INSERT INTO produtos(nome, imagem, descricao, peso, preco, estoque) VALUES
+('caneta azul', ${imageAddress}, 'caneta de cor azul', 0.1, 1.99, 100),
+('caneta vermelha', ${imageAddress}, 'caneta de cor vermelha', 0.1, 1.59, 200),
+('caneta verde', ${imageAddress}, 'caneta de cor vermelha', 0.2, 2.59, 500)`;
+
+const queryInsertOrders= `INSERT INTO pedidos(dataPedido, parcelas, idComprador, status) VALUES
+('31/12/2021', 12, 1, 'ENVIADO'),
+('01/01/2022', 2, 2, 'EM SEPARAÇÃO'),
+('11/01/2022', 1, 1, 'EM PROCESSAMENTO')`;
+
+const queryInsertOrdersProducts = `INSERT INTO pedidos_produtos(idProduto, quantidade, idVenda) VALUES
+(1, 5, 1),
+(2, 10, 1),
+(3, 2, 1),
+(1, 1, 2),
+(2, 1, 2),
+(3, 1, 2),
+(1, 15, 3),
+(2, 15, 3),
+(3, 15, 3)`;
+
 db.serialize(() => {
   // Queries scheduled here will be serialized.
-  db.run('CREATE TABLE IF NOT EXISTS greetings(message text)')
-    .run(`INSERT INTO greetings(message)
-          VALUES('Hi'),
-                ('Hello'),
-                ('Welcome')`)
-    .all(`SELECT message FROM greetings`, (err, rows) => {
+  db.run(queryCreateTableAddress)
+    .run(queryCreateTableClients)
+    .run(queryCreateTableProducts)
+    .run(queryCreateTableOrders)
+    .run(queryCreateTableOrdersProducts)
+    .run(`INSERT INTO clients(nomeCompleto, email, cpf, dataNascimento, addressId)
+          VALUES('Guilherme', 'guihjr', '098765132', '12/07/1992', 1),
+                ('Alceu', 'guihjr', '098765132', '12/07/1992', 2),
+                ('Rogerio', 'guihjr', '098765132', '12/07/1992', 3)`)
+    .each(`SELECT * FROM clients`, (err, row) => {
       if (err){
         throw err;
       }
-      console.log(rows);
+      console.log(row);
     });
 });
 
